bb2c80376f75241164c564d97c0bc5482ff6e793
Jared <jqrvz6@mst.edu>
2014-09-28 22:37:25 -0500

    Stuff..

--- AI.py
+++ AI.py
@@ -1,7 +1,7 @@
 #-*-python-*-
 from BaseAI import BaseAI
 from GameObject import *
-import random
+from random  import *
 
 class AI(BaseAI):
   """The class implementing gameplay logic."""
@@ -16,12 +16,26 @@ class AI(BaseAI):
   CLAW, ARCHER, REPAIRER, HACKER, TURRET, WALL, TERMINATOR, HANGAR = range(8)
 
   def spawn_units(self):
-    self.players[self.playerID].orbitalDrop(0,0, random.randint(0,7))
+    if self.playerID == 0:
+	  x = 0
+	  y = randint(0, self.mapHeight-1)
+    else:
+	  x = self.mapWidth-1
+	  y = randint(0, self.mapHeight-1)
+	  
+    self.players[self.playerID].orbitalDrop(x,y, randint(0,7))
 
   def move_units(self):
-    for droid in self.droids:
-      droid.move(droid.x+1, droid.y)
-      print "Droid moved to (%s, %s)" % (droid.x, droid.y)
+    if self.playerID == 0:
+      for droid in self.droids:
+        droid.move(droid.x+1, droid.y)
+        print "Droid moved to (%s, %s)" % (droid.x, droid.y)
+  	  
+    elif self.playerID == 1:
+      for droid in self.droids:
+        droid.move(droid.x+1, droid.y)
+        print "Droid moved to (%s, %s)" % (droid.x, droid.y)
+	  
 
   def init(self):
     pass
@@ -31,7 +45,9 @@ class AI(BaseAI):
 
   ##This function is called once, before your first turn
   def run(self):
-    self.spawn()
+    self.spawn_units()
+    self.move_units()
+    return 1
 
   def __init__(self, conn):
     BaseAI.__init__(self, conn)
