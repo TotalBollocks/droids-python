3f0d306894acd5e2f2cf18847e0291875e72b53d
Bollocks <jqrainwater@yahoo.com>
2014-10-10 15:04:21 -0500

    Copied Russley

--- AI.py
+++ AI.py
@@ -1,7 +1,7 @@
 #-*-python-*-
 from BaseAI import BaseAI
 from GameObject import *
-from random  import *
+import random
 
 class AI(BaseAI):
   """The class implementing gameplay logic."""
@@ -17,36 +17,42 @@ class AI(BaseAI):
 
   def spawn_units(self):
     if self.playerID == 0:
-	  x = 0
-	  y = randint(0, self.mapHeight-1)
-    else:
-	  x = self.mapWidth-1
-	  y = randint(0, self.mapHeight-1)
-	  
-    self.players[self.playerID].orbitalDrop(x,y, randint(0,7))
+      x = 0
+      y = random.randint(0, self.mapHeight-1)
+      variant = random.randint(0,7)
+      self.players[self.playerID].orbitalDrop(x,y,variant)
+    elif self.playerID == 1:
+      x = self.mapWidth-1
+      y = random.randint(0, self.mapHeight-1)
+      variant = random.randint(0,7)
+      self.players[self.playerID].orbitalDrop(x,y,variant)  
+    return
 
   def move_units(self):
     if self.playerID == 0:
       for droid in self.droids:
-        droid.move(droid.x+1, droid.y)
-        print "Droid moved to (%s, %s)" % (droid.x, droid.y)
-  	  
+        droid.move( droid.x +1 , droid.y)
     elif self.playerID == 1:
       for droid in self.droids:
-        droid.move(droid.x+1, droid.y)
-        print "Droid moved to (%s, %s)" % (droid.x, droid.y)
-	  
+        droid.move( droid.x -1 , droid.y)
 
+    return
+
+  ##This function is called once, before your first turn
   def init(self):
     pass
 
+  ##This function is called once, after your last turn
   def end(self):
     pass
 
-  ##This function is called once, before your first turn
+  ##This function is called each time it is your turn
+  ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
     self.spawn_units()
+
     self.move_units()
+
     return 1
 
   def __init__(self, conn):
